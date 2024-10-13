5435da00c5a4555814936f8b03560358a2a76dc8
ManTea <mank18fpter@gmail.com>
2024-10-13 18:13:43 +0700

    API GenAI, Indexing, Storing DB in local with FAISS

---
+++ BE/models/read_files.py
@@ -0,0 +1,35 @@
+#@title Setting up the Auth
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Qdrant
+import google.generativeai as genai
+# import qdrant_client
+import sys
+import io
+import os
+
+os.environ["GOOGLE_API_KEY"] = 'AIzaSyBy7olYxlX8bHNXZ3AffnvR8FNth7KRIQ8'
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def read_local():
+    path_pdf = "D:/workspace/translator-app-gpt-4o-streamlit-main/datasource/file_pdf"
+    pdf_loader = PyPDFDirectoryLoader(path_pdf, extract_images= False)
+
+    docs = pdf_loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 300)
+
+    docs = text_splitter.split_documents(docs)
+
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    db = FAISS.from_documents(
+        documents = docs,
+        embedding=embeddings # passing in the embedder model
+    )
+    db.save_local('BE/embedding')
+
+read_local()
